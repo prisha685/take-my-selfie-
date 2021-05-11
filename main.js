0c7176039fb7d5672b2f9effdecf7a586d3b2e62
prisha685 <77448105+prisha685@users.noreply.github.com>
2021-05-11 19:41:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recogntion=new SpeechRecognition();
+function start() {
+    document.getElementById("textbox").innerHTML="";
+    recogntion.start();
+}
+recogntion.onresult=function run(event) {
+    console.log(event);
+    var Content=event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML=Content;
+    speak();
+    }
+    function speak() {
+        var synth=window.speechSynthesis;
+        speak_data=document.getElementById("textbox").value;
+        var utterThis=new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+        Webcam.attach( camera );
+    }
+    Webcam.set({
+        width: 320,
+        height: 240,
+        image_format: 'png',
+        png_quality: 90
+      });
+      camera =document.getElementById("camera");
